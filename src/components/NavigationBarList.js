a20733b1d4d3db53186bd231c1837a85dde98b88
Himanshu Rana <himanshu.rana2111@gmail.com>
2019-05-19 14:29:13 +0530

    installed FontAwesome library, included icons into navigation bar, navigation JSON file, social media icons, initab logo

--- src/components/NavigationBarList.js
+++ src/components/NavigationBarList.js
@@ -1,9 +1,10 @@
 import React from "react";
 import NavigationBarListItem from "./NavigationBarListItem";
+import "../css/navigation-bar.css";
 
 const NavigationBarList = props => {
   return (
-    <div>
+    <div class="navigation-bar-list-box">
       <NavigationBarListItem navigationBarData={props.navigationBarData} />
     </div>
   );
--- src/components/NavigationBarListItem.js
+++ src/components/NavigationBarListItem.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "../css/navigation-bar.css";
 
 const NavigationBarListItem = props => {
-  return props.navigationBarData.map(data => {
+  return props.navigationBarData.map((data, i) => {
     return (
-      <div>
-        <span>{data.icon}</span>
-        <span>{data.title}</span>
+      <div class="navigation-bar-list-item-box" key={i}>
+        <span>
+          <FontAwesomeIcon icon={[data.prefix, data.icon]} size="2x" class="navigation-bar-icon" style={{backgroundColor: data.color}} />
+        </span>
+        <span class="navigation-bar-text" style={{color: data.color}}>{data.title}</span>
+        {data.lockIcon? <FontAwesomeIcon icon="lock" style={{color: "white", marginLeft: 5}}/> : "" }
+        
       </div>
     );
   });
