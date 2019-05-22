463d7d1eb3901a6ef56cc87c1db0a4a6f5a85c77
Himanshu Rana <himanshu.rana2111@gmail.com>
2019-05-22 17:22:27 +0530

    Updates

---
+++ precache-manifest.5bc47538d1f52fc445abc192472f7d27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4ac6a1aba168011787cb8358fdbe081",
+    "url": "/initab-extension-page/index.html"
+  },
+  {
+    "revision": "965716d8bc82bb3439e6",
+    "url": "/initab-extension-page/static/css/main.3420c0a6.chunk.css"
+  },
+  {
+    "revision": "412f444b80681482fd5b",
+    "url": "/initab-extension-page/static/js/2.2f4292f8.chunk.js"
+  },
+  {
+    "revision": "965716d8bc82bb3439e6",
+    "url": "/initab-extension-page/static/js/main.b6494481.chunk.js"
+  },
+  {
+    "revision": "9d1e7f44767576a46150",
+    "url": "/initab-extension-page/static/js/runtime~main.0bd5f240.js"
+  }
+]);
